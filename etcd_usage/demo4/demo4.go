b04703e339c5a4917f915441f14f158dfd2b49b0
ssuchichan <ssuchichan@163.com>
2018-12-24 21:24:08 +0800

    获取指定目录(前缀)下的值

---
+++ etcd_usage/demo4/demo4.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"go.etcd.io/etcd/clientv3"
+	"time"
+)
+
+func main() {
+	var (
+		config  clientv3.Config
+		client  *clientv3.Client
+		err     error
+		kv      clientv3.KV
+		getResp *clientv3.GetResponse
+	)
+	// 客户端配置
+	config = clientv3.Config{
+		Endpoints:   []string{"127.0.0.1:2379"},
+		DialTimeout: 5 * time.Second,
+	}
+	// 建立客户端
+	if client, err = clientv3.New(config); err != nil {
+		fmt.Println("clientv3 new err: ", err)
+		return
+	}
+	// 用于读写etcd键值对
+	kv = clientv3.NewKV(client)
+	// 写入一个job
+	kv.Put(context.TODO(), "/cron/jobs/job3", "good")
+	// 读取/cron/jobs/为前缀的所有key
+	if getResp, err = kv.Get(context.TODO(), "/cron/jobs/", clientv3.WithPrefix()); err != nil {
+		fmt.Println("get err:", err)
+		return
+	}
+	for _, v := range getResp.Kvs {
+		fmt.Println(v)
+	}
+
+}
