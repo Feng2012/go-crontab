e3672fe9400c9d37fa450f20a4cf80ea33f6d5d0
ssuchichan <ssuchichan@163.com>
2018-12-25 17:06:46 +0800

    监听kv的变化

---
+++ etcd_usage/demo7/demo7.go
@@ -0,0 +1,72 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"go.etcd.io/etcd/clientv3"
+	"time"
+)
+
+func main() {
+	var (
+		client             *clientv3.Client
+		err                error
+		getResp            *clientv3.GetResponse
+		watcher            clientv3.Watcher
+		watchStartRevision int64
+		watchRespChan      <-chan clientv3.WatchResponse
+		watchResp          clientv3.WatchResponse
+		event              *clientv3.Event
+	)
+
+	// 客户端配置
+	config := clientv3.Config{
+		Endpoints:   []string{"127.0.0.1:2379"},
+		DialTimeout: 5 * time.Second,
+	}
+
+	// 建立连接
+	if client, err = clientv3.New(config); err != nil {
+		fmt.Println("clientv3 new err: ", err)
+		return
+	}
+
+	// KV
+	kv := clientv3.NewKV(client)
+	// 模拟etcd中KV的变化
+	go func() {
+		for {
+			kv.Put(context.TODO(), "/cron/jobs/job7", "i am job7")
+			kv.Delete(context.TODO(), "/cron/jobs/job7")
+			time.Sleep(1 * time.Second)
+		}
+	}()
+
+	// Get到当前的值
+	if getResp, err = kv.Get(context.TODO(), "/cron/jobs/job7"); err != nil {
+		fmt.Println("kv get err:", err)
+		return
+	}
+	// 当前key存在
+	if len(getResp.Kvs) != 0 {
+		fmt.Println("当前值:", string(getResp.Kvs[0].Value))
+	}
+	// 当前etcd集群事务ID，单调递增
+	watchStartRevision = getResp.Header.Revision + 1
+	// 创建watcher
+	watcher = clientv3.NewWatcher(client)
+	// 启动监听
+	fmt.Println("从该版本向后监听:", watchStartRevision)
+	watchRespChan = watcher.Watch(context.TODO(), "/cron/jobs/job7", clientv3.WithRev(watchStartRevision))
+	// 处理kv变化事件
+	for watchResp = range watchRespChan {
+		for _, event = range watchResp.Events {
+			switch event.Type {
+			case 0: // PUT
+				fmt.Println("修改为:", string(event.Kv.Value), " Revision:", event.Kv.CreateRevision, event.Kv.ModRevision)
+			case 1: // DELETE
+				fmt.Println("删除了:", "Revision:", event.Kv.ModRevision)
+			}
+		}
+	}
+}
